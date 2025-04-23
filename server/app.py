ba43c210a9970cd8abe210289da0198f5d042878
KerissaGit <kerissa525@gmail.com>
2025-04-23 17:06:52 -0400

    Added base app.py

--- server/app.py
+++ server/app.py
@@ -9,15 +9,73 @@ from flask_cors import flask_cors
 
 # Local imports
 from config import app, db, api, bcrypt
-from models import 
-# Add your model imports
+from models import User, ItemsCart, Grocery, Deli
 
 
-# Views go here!
 
-@app.route('/')
-def index():
-    return '<h1>Project Server</h1>'
+class Index(Resource):
+    def get(self):
+        return '<h1>Market Mate Project Server<h1>'
+
+
+class Users(Resource):
+    def get(self):
+        users = db.session.execute(db.select(User)).scalars().all()
+        users_list = [user.to_dict() for user in users]
+        print("Session after login:", dict(session))
+
+        return make_response({"users": users_list}, 200)
+
+
+class ItemsCarts(Resource):
+    def get(self):
+        itemscarts = db.session.execute(db.select(ItemsCart)).scalars().all()
+        return make_response([itemscart.to_dict() for itemscart in itemscarts], 200)
+
+
+# Creating this to prep for use later
+# Might be used for Grocery OR ItemsCart
+# class ItemsById(Resource):
+#     def get(self, id):
+#         try:
+#             'Created class to be used for later'
+#             return make_response( .to_dict(), 200)
+#         except NoResultFound:
+#             return make_response({"error": "Item not found."}, 404)
+
+
+
+class Groceries(Resource):
+    def get(self):
+        groceries = db.session.execute(db.select(Grocery)).scalars().all()
+        return make_response([grocery.to_dict() for grocery in groceries], 200)
+
+
+
+class Deli(Resource):
+    def get(self):
+        delis = db.session.execute(db.select(Deli).scalars().all()
+        return make_response([deli.to_dict() for deli in delis], 200)
+
+
+        
+
+
+# Routing
+api.add_resource(Index, '/')
+api.add_resource(Users, '/users')
+api.add_resource(ItemsCarts, '/itemcarts')
+# api.add_resource(ItemsById, '/itemcarts/<int:id>')
+api.add_resource(Groceries, '/groceries')
+api.add_resource(Delis, '/delis')
+
+
+
+
+# Came with project template
+# @app.route('/')
+# def index():
+#     return '<h1>Project Server</h1>'
 
 
 if __name__ == '__main__':
--- server/seed.py
+++ server/seed.py
@@ -2,13 +2,25 @@
 
 # Standard library imports
 from random import randint, choice as rc
+import random
 
 # Remote library imports
 from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from config import db
+from models import db, User, Grocery, Deli, ItemsCart
+
+
+with app.app_context():
+    print("🌱 Seeding database...")
+
+    db.drop_all()
+    db.create_all()
+
+
+
 
 if __name__ == '__main__':
     fake = Faker()
