87c7fd1fe26c48d136bdbadb10324974d1b043f3
KerissaGit <kerissa525@gmail.com>
2025-04-23 17:29:51 -0400

    Added Base seed.py

--- server/config.py
+++ server/config.py
@@ -1,14 +1,12 @@
-# Standard library imports
-
-# Remote library imports
+# Imports
 from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from flask_bcrypt import Bcrypt
 
-# Local imports
 
 # Instantiate app, set attributes
 app = Flask(__name__)
@@ -19,6 +17,10 @@ app.json.compact = False
 # Define metadata, instantiate db
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "pk": "pk_%(table_name)s"
 })
 db = SQLAlchemy(metadata=metadata)
 migrate = Migrate(app, db)
@@ -26,6 +28,12 @@ db.init_app(app)
 
 # Instantiate REST API
 api = Api(app)
+bcrypt = Bcrypt(app)
 
 # Instantiate CORS
 CORS(app)
+
+# Secret key for session management (Test from NGP)
+# app.secret_key = b'E%t\x95o\x84\x01|8b\xd3\xfa\xf9!\xeb\xf1'
+# CORS(app, supports_credentials=True)
+
--- server/seed.py
+++ server/seed.py
@@ -20,6 +20,56 @@ with app.app_context():
     db.create_all()
 
 
+# Creating hard coded groceries to test
+def create_groceries():
+    groceries = [
+        Grocery(
+            name="Bread"
+            description="Wheat"
+            quantity=1
+        ),
+        Grocery(
+            name="Cheese"
+            description="Mozzarella"
+            quantity=2
+        ),
+        Grocery(
+            name="Meat"
+            description="Turkey"
+            quantity=1
+        ),
+    ]
+    return groceries
+
+
+def create_users():
+    users = []
+    for _ in range(5):
+        user = User(
+            username=fake.user_name(),
+            email=fake.email()
+        )
+        user.password_hash = "password123"
+        users.append(user)
+
+
+# Needs to be updated and modified
+# def create_items_cart(groceries, deli):
+#     itemcarts = []
+#     for _ in range(15):
+#         name = fake.name()
+
+#         itemscart = ItemsCart(
+#             grocery_id=randint(1, len(groceries))
+#             deli_id=randint(1, len(delis))
+#         )
+#         itemcarts.append(itemscart)
+
+#     return itemscarts
+
+
+
+
 
 
 if __name__ == '__main__':
