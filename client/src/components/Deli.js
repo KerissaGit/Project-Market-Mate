6ad853c84cf6dc487140845e11905dbb1574b770
KerissaGit <kerissa525@gmail.com>
2025-05-06 10:52:49 -0400

    Fixed Deli to update with Users-2

--- client/src/components/Deli.js
+++ client/src/components/Deli.js
@@ -1,9 +1,5 @@
 import React, { useState } from "react";
 
-function Deli() {
-    const [bread, setBread] = useState("");
-    const [cheese, setCheese] = useState("");
-    const [meat, setMeat] = useState("");
 
 function Deli() {
     const [bread, setBread] = useState("");
@@ -12,7 +8,6 @@ function Deli() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-      
         const newDeli = {
             bread_type: bread,
             cheese_type: cheese,
@@ -51,6 +46,7 @@ function Deli() {
                     <option value="White">White</option>
                     <option value="Spinach Wrap">Spinach Wrap</option>
                 </select>
+
                 <label>Meat Type:</label>
                 <select value={meat} onChange={(e) => setMeat(e.target.value)} required>
                     <option value="">Select Meat</option>
@@ -58,6 +54,7 @@ function Deli() {
                     <option value="Ham">Ham</option>
                     <option value="Chicken">Chicken</option>
                 </select>
+
                 <label>Cheese Type:</label>
                 <select value={cheese} onChange={(e) => setCheese(e.target.value)} required>
                     <option value="">Select Cheese</option>
@@ -65,8 +62,8 @@ function Deli() {
                     <option value="Swiss">Swiss</option>
                     <option value="Provolone">Provolone</option>
                 </select>
-                <button type="submit" className="action-button">Add Custom Item to Cart</button>
 
+                <button type="submit" className="action-button">Add Custom Item to Cart</button>
             </form>
         </div>
     );
